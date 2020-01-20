41072ed297a83f53f1cd5a8c389f2ac4bf995e9a
onsing <onsing@microsoft.com>
2020-01-20 17:07:47 +0530

    UI Fixes

--- Facebook/WebApp/ClientJs/ConfigureController.js
+++ Facebook/WebApp/ClientJs/ConfigureController.js
@@ -4,30 +4,28 @@
 
     var checkSharedSecret = "setup/Validate";
     var configurationUrl = "api/Configuration";
-    $scope.login = function () {
+    $scope.login = function() {
         if ($scope.sharedSecretKey != null) {
             $cookies.put("sharedSecret", $scope.sharedSecretKey);
             $cookies.put("tenantId", $scope.tenantId);
-            $http.get(checkSharedSecret).then(function (response) {
+            $http.get(checkSharedSecret).then(function(response) {
                 $scope.isLoginComplete = response.data.Status;
             });
 
-            $http.get(configurationUrl).then(function (response) {
+            $http.get(configurationUrl).then(function(response) {
                 var result = response.data;
                 setTimeout(function () {
                 }, 500);
                 $scope.FBAppIdValue = result["FBAppIdValue"];
-                $scope.FBAppSecretValue = result["FBAppSecretValue"];
+                $scope.FBAppSecretValue = result["FBAppSecretValue"] ;
                 $scope.FBVerifyTokenValue = result["FBVerifyTokenValue"];
                 $scope.AADAppIdValue = result["AADAppIdValue"];
                 $scope.AADAppSecretValue = result["AADAppSecretValue"];
-                $scope.AADAppUriValue = result["AADAppUriValue"];
-                $scope.InstrumentationKeyValue = result["InstrumentationKeyValue"];
             });
         }
     }
-
-    $scope.SaveConfigSettings = function () {
+    
+    $scope.SaveConfigSettings = function() {
         $scope.isDisabledSaveButton = true;
         $scope.configurationSavedMsg = "Saving Configuration ...";
         var settings = {
@@ -35,19 +33,17 @@
             FBAppSecretValue: (typeof $scope.FBAppSecretValue !== 'undefined') ? $scope.FBAppSecretValue : "",
             FBVerifyTokenValue: (typeof $scope.FBVerifyTokenValue !== 'undefined') ? $scope.FBVerifyTokenValue : "",
             AADAppIdValue: (typeof $scope.AADAppIdValue !== 'undefined') ? $scope.AADAppIdValue : "",
-            AADAppSecretValue: (typeof $scope.AADAppSecretValue !== 'undefined') ? $scope.AADAppSecretValue : "",
-            AADAppUriValue: (typeof $scope.AADAppUriValue !== 'undefined') ? $scope.AADAppUriValue : "",
-            InstrumentationKeyValue: (typeof $scope.InstrumentationKeyValue !== 'undefined') ? $scope.InstrumentationKeyValue : ""
+            AADAppSecretValue: (typeof $scope.AADAppSecretValue !== 'undefined') ? $scope.AADAppSecretValue : ""
         };
-        $http.post(configurationUrl, settings).then(function (response) {
+        $http.post(configurationUrl, settings).then(function(response) {
             var res = response.data;
             setTimeout(function () {
             }, 500);
             $scope.configurationSavedMsg = "Configuration Saved Successfully.";
-        }).catch(function (error) { });
+        }).catch(function(error) { });
     }
 
-    $scope.Home = function () {
+    $scope.Home = function() {
         window.location.href = "/index.cshtml";
     }
 
